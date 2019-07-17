5bfd5749e8aff9a0dca0ca87bfe50cbe1cbeccf8
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-07-17 18:44:31 +0530

    controller class

--- spring-hibernate-application/src/main/java/com/zeeshan/controller/UserController.java
+++ spring-hibernate-application/src/main/java/com/zeeshan/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -20,56 +21,63 @@ import com.zeeshan.vo.UserVO;
 @Controller
 public class UserController {
 
+	private static final Logger logger = Logger.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView mainPage() {
+		logger.info("mainPage method");
 		return new ModelAndView("home");
 	}
 
-	@RequestMapping(value = "/index")
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public ModelAndView indexPage() {
+		logger.info("indexPage method");
 		return new ModelAndView("home");
 	}
 
-	@RequestMapping(value = "/user/add")
+	@RequestMapping(value = "/user/add", method = RequestMethod.GET)
 	public ModelAndView addUserPage() {
 
-		System.out.println("controller save method");
+		logger.info("addUserPage method");
 		ModelAndView mav = new ModelAndView("add-user-form");
 		mav.addObject("user", new UserVO());
 		return mav;
 	}
 
-	@RequestMapping(value = "/user/add/process")
+	@RequestMapping(value = "/user/add/process", method = RequestMethod.POST)
 	public ModelAndView addingUser(@ModelAttribute("user") @Valid UserVO user, BindingResult result)
 			throws ParseException {
+		logger.info("addingUser method");
 		ModelAndView mav = new ModelAndView("home");
 		if (result.hasErrors()) {
+			logger.error("user bean has error");
 			return new ModelAndView("add-user-form");
 		}
+
+		logger.info("user bean has no errors");
 		userService.addUser(user);
 
-		/* Period period = userService.calculateDate(stringToDate(user.getDob())); */
 		String message = "user added Successfully";
 		mav.addObject("message", message);
-		/*
-		 * mav.addObject("days", period.getDays()); mav.addObject("months",
-		 * period.getMonths()); mav.addObject("years", period.getYears());
-		 */ return mav;
+		return mav;
 	}
 
-	@RequestMapping(value = "/user/list")
+	@RequestMapping(value = "/user/list", method = RequestMethod.GET)
 	public ModelAndView listOfUsers() {
+		logger.info("listOfUsers method");
 		ModelAndView mav = new ModelAndView("list-of-users");
 		List<UserVO> users = userService.getUsers();
+
 		mav.addObject("users", users);
 		return mav;
 	}
 
 	@RequestMapping(value = "/user/edit/{id}", method = RequestMethod.GET)
 	public ModelAndView editingUser(@PathVariable Integer id) {
+		logger.info("editingUser method");
 		ModelAndView mav = new ModelAndView("edit-user-form");
 		UserVO user = userService.getUser(id);
 		mav.addObject("user", user);
@@ -79,11 +87,14 @@ public class UserController {
 	@RequestMapping(value = "/user/edit/{id}", method = RequestMethod.POST)
 	public ModelAndView editUser(@PathVariable Integer id, @ModelAttribute("user") @Valid UserVO user,
 			BindingResult result) throws ParseException {
+		logger.info("editUser method");
 		ModelAndView mav = new ModelAndView("home");
 		if (result.hasErrors()) {
+			logger.info("user bean has errors");
 			return new ModelAndView("edit-user-form");
 		}
 
+		logger.info("user bean has no errors");
 		userService.updateUser(user);
 		String message = "user edited Successfully";
 		mav.addObject("message", message);
@@ -93,6 +104,7 @@ public class UserController {
 	@RequestMapping(value = "/user/delete/{id}", method = RequestMethod.GET)
 	public ModelAndView deleteUser(@PathVariable Integer id) {
 
+		logger.info("deleteUser method");
 		ModelAndView mav = new ModelAndView("home");
 		userService.deleteUser(id);
 		String message = "user deleted Successfully";
