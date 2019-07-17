984eff53cec4c9d04530dcb83bc1947bd6cd1756
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-07-17 18:46:01 +0530

    Date utility class

--- spring-hibernate-application/src/main/java/com/zeeshan/util/DateUtil.java
+++ spring-hibernate-application/src/main/java/com/zeeshan/util/DateUtil.java
@@ -1,21 +1,28 @@
 package com.zeeshan.util;
 
 import java.util.Date;
+
+import org.apache.log4j.Logger;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class DateUtil {
 
+	private static final Logger logger = Logger.getLogger(DateUtil.class);
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
 	public static String dateToString(Date date) {
 
+		logger.info("dateToString method");
 		String strDate = sdf.format(date);
 		return strDate;
 	}
 
 	public static Date stringToDate(String strDate) throws ParseException {
 
+		logger.info("stringToDate method");
 		Date date = sdf.parse(strDate);
 		return date;
 	}
